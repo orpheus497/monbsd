525284569be5e837e676e0271bac48ed747e82a0
orpheus497 <orpheus497@gmail.com>
2026-03-23 21:58:40 +1100

    build: Begin deprecating old files in place of new multi-code-lang build; breakdown of monolithic code

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+set -eu
+
+echo ">> Forging monbsd project structure..."
+
+mkdir -p src include tests docs
+
+touch README.md 
+touch Makefile
+touch .gitignore
+
+echo ">> Structure forged successfully."
+
+# :)
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+  printf("monbsd core engine v0.2.0 initialised.\n");
+  return 0;
+}
